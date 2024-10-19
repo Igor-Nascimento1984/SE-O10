4feea96bff7a86ee540cf2e32bc13c5d073b3fbb
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-10-19 00:57:50 -0300

    Final da aula89pt02

--- src/application01/Program.java
+++ src/application01/Program.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner edd = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
-		
+
 		int n = edd.nextInt();
-		
+
 		double[] vect = new double[n];
-		
-		for (int i = 0;i<n;i++) {
-			vect[i] = edd.nextDouble(); 
+
+		for (int i = 0; i < n; i++) {
+			vect[i] = edd.nextDouble();
 		}
-		
+
 		double soma = 0.0;
-		for (int i = 0;i<n;i++) {
-			soma += vect[i]; 
+		for (int i = 0; i < n; i++) {
+			soma += vect[i];
 		}
-		
-		double media = soma/n; 
-		System.out.println("VALOR TOTAL: "+ soma);
-		System.out.printf("MEDIA DOS VALORES: %.2f%n",media);
-		
+
+		double media = soma / n;
+		System.out.println("VALOR TOTAL: " + soma);
+		System.out.printf("MEDIA DOS VALORES: %.2f%n", media);
+
 		edd.close();
 
 	}
--- src/application02/Program.java
+++ src/application02/Program.java
@@ -3,15 +3,36 @@ package application02;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities01.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner edd = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
-		
+
 		int n = edd.nextInt();
-		
-		
+
+		Product[] vect = new Product[n];
+
+		for (int i = 0; i < vect.length; i++) {
+			edd.nextLine();
+			String nome = edd.nextLine();
+			double preco = edd.nextDouble();
+
+			vect[i] = new Product(nome, preco);
+		}
+
+		double soma = 0.0;
+
+		for (int i = 0; i < vect.length; i++) {
+			soma += vect[i].getPreco();
+		}
+
+		double media = soma / n;
+		System.out.println("VALOR TOTAL: " + soma);
+		System.out.printf("MEDIA DOS VALORES: %.2f%n", media);
+
 		edd.close();
 
 	}
