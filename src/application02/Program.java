244089e1bed3f5ec73a85f503c7319c5892c97be
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-10-19 00:31:29 -0300

    AULA89PT02

---
+++ src/application02/Program.java
@@ -0,0 +1,19 @@
+package application02;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner edd = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		int n = edd.nextInt();
+		
+		
+		edd.close();
+
+	}
+
+}
