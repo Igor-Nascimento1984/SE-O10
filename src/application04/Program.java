10c7e712619aafd2d68872bc6e29cbf2a29e6536
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-10-20 20:27:44 -0300

    Final da aula91ex02

--- src/application04/Program.java
+++ src/application04/Program.java
@@ -1,10 +1,40 @@
 package application04;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-				
+		Scanner edd = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		int n = edd.nextInt();
+
+		double[] vect = new double[n];
+
+		for (int i = 0; i < n; i++) {
+			vect[i] = edd.nextDouble();
+		}
+
+		double soma = 0.0;
+		for (int i = 0; i < n; i++) {
+			soma += vect[i];
+		}
+
+		double media = soma / n;
+		
+		System.out.print("VALORES = ");
+		for (int i = 0; i < n; i++) {
+			System.out.print(" "+vect[i]);;
+			
+		}
+		System.out.println();
+		System.out.println("VALOR TOTAL: " + soma);
+		System.out.printf("MEDIA DOS VALORES: %.2f%n", media);
+
+		edd.close();	
 				
 
 	}
