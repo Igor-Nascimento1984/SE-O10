e3171810b17316456313440fd0636105bfd9932b
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-10-20 18:19:25 -0300

    Final da aula91ex01

--- src/application03/Program.java
+++ src/application03/Program.java
@@ -1,10 +1,29 @@
 package application03;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner edd = new Scanner(System.in);
+		System.out.print("QUANTIDADES DE NUMEROS:");
+		int n = edd.nextInt();
+		
+		int[] vect = new int[n];
+		
+		for (int i = 0;i<vect.length;i++) {
+			System.out.print("NUMEROS: ");
+			vect[i] = edd.nextInt();
+		}
+		System.out.println("NUMEROS NEGATIVOS: ");
+		for (int i = 0;i<vect.length;i++) {
+			if(vect[i]<0) {
+			System.out.println(vect[i]);}
+		}
+				
+				
+		edd.close();
 	}
 
 }
