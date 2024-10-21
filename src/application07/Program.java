1c41335caadcb89cd761e6443d2bf857c7b7e932
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-10-21 13:45:46 -0300

    Final da aula91ex05

--- src/application07/Program.java
+++ src/application07/Program.java
@@ -1,10 +1,37 @@
 package application07;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
+
+		Scanner edd = new Scanner(System.in);
+		System.out.print("QUANTOS NUMEROS VOCE VAI DIGITAR: ");
+		int n = edd.nextInt();
+
+		double[] vect = new double[n];
+
+		for (int i = 0; i < vect.length; i++) {
+			System.out.print("DIGITE " + (i + 1) + "° UM NUMERO:  ");
+			vect[i] = edd.nextDouble();
+		}
+
+		double maiorNumero = vect[0];
+		int posicaoMaior = 0;
+
+		for (int i = 0; i < vect.length; i++) {
+			if (vect[i] > maiorNumero) {
+
+				maiorNumero = vect[i];
+				posicaoMaior = i+1;
+
+			}
+		}
+
+		System.out.println("O maior número é: " + maiorNumero);
+		System.out.println("A posição do maior número é: " + posicaoMaior);
+		edd.close();
 
 	}
 
