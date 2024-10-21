18b88397a4931f255591cfb9688441f287031069
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-10-21 17:20:14 -0300

    Final da aula91ex06

--- src/application08/Program.java
+++ src/application08/Program.java
@@ -1,10 +1,38 @@
 package application08;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
+
+		Scanner edd = new Scanner(System.in);
+		System.out.print("QUANTOS NUMEROS VOCE VAI DIGITAR: ");
+		int n = edd.nextInt();
+
+		double[] vectA = new double[n];
+		double[] vectB = new double[n];
+
+		System.out.println("DIGITE OS VALORES DE VETOR A:");
+
+		for (int i = 0; i < vectA.length; i++) {
+
+			vectA[i] = edd.nextDouble();
+		}
+		System.out.println("DIGITE OS VALORES DE VETOR B:");
+
+		for (int j = 0; j < vectB.length; j++) {
+
+			vectB[j] = edd.nextDouble();
+		}
+
+		for (int m = 0; m < n; m++) {
+
+			double soma = vectA[m] + vectB[m];
+			System.out.println(soma);
+		}
+
+		edd.close();
 
 	}
 
