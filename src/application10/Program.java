f1de0dbb228b7773b7c1ea43a4a96995ef14c493
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-10-26 11:34:12 -0300

    Final da aula91ex08

--- src/application10/Program.java
+++ src/application10/Program.java
@@ -1,11 +1,43 @@
 package application10;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
+
+		Scanner edd = new Scanner(System.in);
+		System.out.print("QUANTOS NUMEROS VOCE VAI DIGITAR: ");
+		int n = edd.nextInt();
+
+		int[] vect = new int[n];
+
+		int soma = 0;
+		int divisor = 0;
+
+		for (int i = 0; i < vect.length; i++) {
+			System.out.print("DIGITE UM NUMERO:  ");
+			vect[i] = edd.nextInt();
+			if (vect[i] % 2 == 0) {
+				soma += vect[i];
+				divisor += 1;
+
+			}
+		}
+
+		if (divisor > 0) {
+			int media = soma / divisor;
+
+			System.out.println(soma);
+			System.out.println(divisor);
+
+			System.out.println("MEDIA NUMEROS PARES: ");
+			System.out.println(media);
+		} else {
+			System.out.println("NÃO TEM NUMERO PARES");
+		}
+
+		edd.close();
 
 	}
-
 }
