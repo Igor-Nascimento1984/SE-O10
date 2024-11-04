89719c73b4cb21ec8d0349d3f2625a39a0f07f97
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-11-04 12:07:37 -0300

    Final da aula95-desafio

--- src/application14/Program.java
+++ src/application14/Program.java
@@ -1,10 +1,42 @@
 package application14;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
+
+		Scanner edd = new Scanner(System.in);
+
+		System.out.print("QUANTOS QUARTOS SERÃO ALUGADOS?");
+		int n = edd.nextInt();
+
+		int[] quarto = new int[n];
+		String[] nome = new String[n];
+		String[] email = new String[n];
+
+		for (int i = 0; i < n; i++) {
+
+			System.out.println("ALUGUEL #" + (i + 1));
+			System.out.print("NOME: ");
+			edd.nextLine();
+			nome[i] = edd.nextLine();
+
+			System.out.print("EMAIL: ");
+			email[i] = edd.nextLine();
+			System.out.print("QUARTO: ");
+			quarto[i] = edd.nextInt();
+
+		}
+
+		System.out.println("SALAS ALUGADAS:");
+
+		for (int i = 0; i < n; i++) {
+
+			System.out.println(quarto[i] + " - " + nome[i] + " - " + email[i]);
+		}
+
+		edd.close();
 
 	}
 
