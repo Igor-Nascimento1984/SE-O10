48751fcc308abd1fe88e397383caa49cec25ff60
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-11-25 19:49:19 -0300

    Final da aula101

--- src/application18/Program.java
+++ src/application18/Program.java
@@ -8,8 +8,33 @@ public class Program {
 		
 		Scanner edd = new Scanner(System.in);
 		
+		int n = edd.nextInt();
+
+		int [][] mat = new int[n] [n];
 		
+		for (int i = 0;i<n;i++) {
+			for (int j = 0;j<n;j++) {
+				mat [i][j] = edd.nextInt();
+			}
+		}
 		
+		for (int i = 0;i<n;i++){
+			
+			System.out.print(mat[i][i]+" ");
+		} 
+		
+		int count = 0;
+		System.out.println();
+		
+		for (int i = 0;i<n;i++) {
+			for (int j = 0;j<n;j++) {
+				if (mat[i][j]<0) {
+					count++;
+				}
+			}
+		}
+		
+		System.out.println("NÚMEROS NEGATIVOS = "+count);
 		edd.close();
 
 	}
