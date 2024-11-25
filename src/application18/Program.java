05b442b737091cf6a0c0c4a03f8df305bbe48b80
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-11-25 19:20:10 -0300

    AULA101

---
+++ src/application18/Program.java
@@ -0,0 +1,17 @@
+package application18;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner edd = new Scanner(System.in);
+		
+		
+		
+		edd.close();
+
+	}
+
+}
