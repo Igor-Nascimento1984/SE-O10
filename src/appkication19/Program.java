0df59c6e7faf5d5a957fcf69cba0f8363aa92558
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-11-27 11:02:51 -0300

    AULA104

---
+++ src/appkication19/Program.java
@@ -0,0 +1,10 @@
+package appkication19;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- src/application18/Program.java
+++ src/application18/Program.java
@@ -5,36 +5,35 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner edd = new Scanner(System.in);
-		
+
 		int n = edd.nextInt();
 
-		int [][] mat = new int[n] [n];
-		
-		for (int i = 0;i<n;i++) {
-			for (int j = 0;j<n;j++) {
-				mat [i][j] = edd.nextInt();
+		int[][] mat = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				mat[i][j] = edd.nextInt();
 			}
 		}
-		
-		for (int i = 0;i<n;i++){
-			
-			System.out.print(mat[i][i]+" ");
-		} 
-		
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+
 		int count = 0;
 		System.out.println();
-		
-		for (int i = 0;i<n;i++) {
-			for (int j = 0;j<n;j++) {
-				if (mat[i][j]<0) {
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (mat[i][j] < 0) {
 					count++;
 				}
 			}
 		}
-		
-		System.out.println("NÚMEROS NEGATIVOS = "+count);
+
+		System.out.println("NÚMEROS NEGATIVOS = " + count);
 		edd.close();
 
 	}
