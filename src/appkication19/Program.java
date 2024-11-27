0ca688d8eab88c6144aa463e973b8560304f9a0b
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2024-11-27 11:36:22 -0300

    Final da aula104

--- src/appkication19/Program.java
+++ src/appkication19/Program.java
@@ -1,9 +1,58 @@
 package appkication19;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Scanner edd = new Scanner(System.in);
+		System.out.print("LINHAS: ");
+		int n = edd.nextInt();
+		System.out.print("COLUNAS: ");
+		int m = edd.nextInt();
+		int[][] mat = new int[n][m];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				mat[i][j] = edd.nextInt();
+			}
+		}
+
+		int count = 0;
+		System.out.println();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (mat[i][j] < 0) {
+					count++;
+				}
+			}
+		}
+		System.out.print("ESCOLHA O NÚMERO:");
+		int x = edd.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (mat[i][j] == x) {
+					System.out.println("Position " + i + "," + j + ":");
+					if (j > 0) {
+						System.out.println("Left: " + mat[i][j - 1]);
+					}
+					if (i > 0) {
+						System.out.println("Up: " + mat[i - 1][j]);
+					}
+					if (j < mat[i].length - 1) {
+						System.out.println("Right: " + mat[i][j + 1]);
+					}
+					if (i < mat.length - 1) {
+						System.out.println("Down: " + mat[i + 1][j]);
+					}
+				}
+			}
+		}
+
+		System.out.println("NÚMEROS NEGATIVOS = " + count);
+		edd.close();
 
 	}
 
